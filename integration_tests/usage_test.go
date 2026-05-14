cbadc449aa093e46ea0db268e6d91349a499935d
Jonathan Harden <jonathan.harden@digital.cabinet-office.gov.uk>
2026-05-14 09:16:07 +0100

    Add go test ci workflow

--- integration_tests/usage_test.go
+++ integration_tests/usage_test.go
@@ -2,6 +2,8 @@ package integration_tests
 
 import (
 	"context"
+	"errors"
+	"os/exec"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -15,6 +17,22 @@ var _ = Describe("Usage", func() {
 
 			err = cmd.Run()
 			Expect(err).NotTo(HaveOccurred())
+		})
+	})
+
+	Context("when executed with an invalid argument", func() {
+		It("Exist unsuccessfully", func() {
+			cmd, err := cliCmd(context.Background())
+			Expect(err).NotTo(HaveOccurred())
+
+			cmd.Args = append(cmd.Args, "--wibble")
+			err = cmd.Run()
+			Expect(err).To(HaveOccurred())
+
+			exitErr, ok := errors.AsType[*exec.ExitError](err)
+			Expect(ok).To(BeTrueBecause("The reason the command failed was that the govuk cli binary exited non-zero"))
+
+			Expect(exitErr.ExitCode()).NotTo(BeZero())
 		})
 	})
 })
