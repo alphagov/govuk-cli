9a47c6f8ee56b4b15b1e66c702bf07754a141f91
Sam Simpson <sam@simpson.id>
2026-07-20 13:54:25 +0100

    Add style package
    This package includes some standard lipgloss styling for the CLI
    
    This includes GOV.UK's blue colour and a table for displaying key-value pairs to the user

---
+++ internal/style/style.go
@@ -0,0 +1,34 @@
+package style
+
+import (
+	"charm.land/lipgloss/v2"
+	"charm.land/lipgloss/v2/table"
+)
+
+var CommandStyle = lipgloss.NewStyle().PaddingLeft(1)
+var BoldStyle = lipgloss.NewStyle().Bold(true)
+var GovukBlue = lipgloss.Color("#1d70b8")
+
+// returns a lipgloss table designed for displaying key/value pairs
+func KVTable() *table.Table {
+	t := table.New().
+		Border(lipgloss.NormalBorder()).
+		BorderStyle(lipgloss.NewStyle().Foreground(GovukBlue)).
+		StyleFunc(kvTableStyleFunc)
+	return t
+}
+
+var kvTableBaseStyle = lipgloss.NewStyle().
+	Padding(0, 1)
+var kvTableKeyStyle = kvTableBaseStyle.
+	Bold(true).
+	Align(lipgloss.Left)
+
+func kvTableStyleFunc(row int, col int) lipgloss.Style {
+	switch col {
+	case 0:
+		return kvTableKeyStyle
+	default:
+		return kvTableBaseStyle
+	}
+}
